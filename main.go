25d7af69144dcf94be765cf6ca13c99efe782b37
Schlypar <schlypar@gmail.com>
2025-03-07 19:57:55 +0300

    new example main for testing

--- main.go
+++ main.go
@@ -1,62 +1,26 @@
 package main
 
 import (
+	"echosphere/apis"
 	wb "echosphere/apis/wb"
-	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
-	// ozon "echosphere/apis/ozon"
 )
 
 func handle_wb(w http.ResponseWriter, r *http.Request) {
-	wb_key := os.Getenv("WB_API_SAFE")
+	// http.Error(w, fmt.Sprintf("Failed to parse JSON response: %v", err), http.StatusInternalServerError)
 
-	isAnswered := "false"
-	take := 2
-	skip := 0
-
-	url := fmt.Sprintf("https://feedbacks-api.wildberries.ru/api/v1/feedbacks?isAnswered=%s&take=%d&skip=%d", isAnswered, take, skip)
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Failed to create request: %v", err), http.StatusInternalServerError)
-		return
+	wb_api := wb.WBAPI{}
+	config := apis.FeedbackRequestConfig{
+		IsAnswered: false,
+		Take:       1,
+		Skip:       0,
+		DateFrom:   nil,
+		DateTo:     nil,
 	}
 
-	req.Header.Set("Authorization", "Bearer "+wb_key)
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Failed to send request: %v", err), http.StatusInternalServerError)
-		return
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Failed to read response body: %v", err), http.StatusInternalServerError)
-		return
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		http.Error(w, fmt.Sprintf("API request failed with status: %s", resp.Status), http.StatusInternalServerError)
-		return
-	}
-
-	var feedback wb.FeedbackResponse
-	if err := json.Unmarshal(body, &feedback); err != nil {
-		http.Error(w, fmt.Sprintf("Failed to parse JSON response: %v", err), http.StatusInternalServerError)
-		return
-	}
-
-	// -------- CHECK HERE --------------
-	condensed := wb.ConvertWbIntoCondensed(feedback)
-	fmt.Printf("%+v\n", condensed)
-	// -------- END CHECK ---------------
+	wb_reviews, _ := wb_api.GetFeedback(config)
+	fmt.Printf("%+v\n", wb_reviews)
 
 	w.WriteHeader(http.StatusOK)
 }
