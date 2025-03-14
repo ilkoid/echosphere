0156d88da1af547bcce14e1d7a838f0404d4d3b8
Schlypar <schlypar@gmail.com>
2025-03-14 20:47:40 +0300

    using reflect for dynamic populating of google sheets

--- google_sheets/sheets.go
+++ google_sheets/sheets.go
@@ -5,6 +5,7 @@ import (
 	"echosphere/apis"
 	"fmt"
 	"os"
+	"reflect"
 
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
@@ -12,11 +13,35 @@ import (
 )
 
 func PrepareDataForSheets(reviews []apis.ReviewCondensed) [][]interface{} {
-	values := [][]interface{}{
-		{"Id", "Text"},
+	if len(reviews) == 0 {
+		return [][]interface{}{}
 	}
+
+	first := reviews[0]
+	structFields := reflect.VisibleFields(reflect.TypeOf(first))
+	structFieldNames := []string{}
+	for _, field := range structFields {
+		if field.Type.Kind() != reflect.Array && field.Type.Kind() != reflect.Struct {
+			structFieldNames = append(structFieldNames, field.Name)
+		}
+	}
+
+	interfacedFieldNames := make([]interface{}, len(structFieldNames))
+	for i, v := range structFieldNames {
+		interfacedFieldNames[i] = v
+	}
+
+	values := [][]interface{}{}
+	values = append(values, interfacedFieldNames)
+
 	for _, review := range reviews {
-		values = append(values, []interface{}{review.ID, review.Text})
+		interfacedFieldsValues := make([]interface{}, len(structFieldNames))
+		currentFields := []reflect.Value{}
+		for i, fieldName := range structFieldNames {
+			interfacedFieldsValues[i] = fmt.Sprintf("%v", reflect.ValueOf(review).FieldByName(fieldName))
+			currentFields = append(currentFields, reflect.ValueOf(review).FieldByName(fieldName))
+		}
+		values = append(values, interfacedFieldsValues)
 	}
 	return values
 }
@@ -44,8 +69,16 @@ func WriteReviewsToSheet(values [][]interface{}, spreadsheetId string, writeRang
 		Values: values,
 	}
 
+	// first of all clear what was before
+	clearRequest := &sheets.ClearValuesRequest{}
+	_, err = srv.Spreadsheets.Values.Clear(spreadsheetId, "Лист1!A1:Z10000", clearRequest).Do()
+	if err != nil {
+		return fmt.Errorf("unable to clear sheet: %v", err)
+	}
+
+	// now write new data
 	_, err = srv.Spreadsheets.Values.Append(spreadsheetId, writeRange, valueRange).
-		ValueInputOption("USER_ENTERED").
+		ValueInputOption("RAW").
 		Do()
 	if err != nil {
 		return fmt.Errorf("unable to write data to sheet: %v", err)
