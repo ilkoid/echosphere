c0e184e561d836529bb8038db3a1d4a642c85590
BaldursFrog <g.kashparov@outlook.com>
2025-04-10 22:33:15 +0300

    added auto getter of Gigachat access key

--- server/routes.go
+++ server/routes.go
@@ -1,13 +1,23 @@
 package server
 
 import (
+	"bytes"
+	"crypto/tls"
 	"echosphere/apis"
 	wb "echosphere/apis/wb"
 	"echosphere/google_sheets"
-
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 )
+
+var gigachatAccessKeyCreationTime time.Time = time.Unix(0, 0)
 
 const (
 	spreadsheetID   = "1TKuEZvJsnqfkolxXtQzDPPUxdtDEVrtgjvCrVhnAA2o"
@@ -20,7 +30,7 @@ func addRoutes(
 	processer ReviewProcesser,
 	repository Repository,
 ) {
-	mux.Handle("/wbfeedback", wbHandler(repository, processer))
+	mux.Handle("/wbfeedback", ValidateOrCreateGigachatAccessKey(wbHandler(repository, processer)))
 }
 
 func wbHandler(repository Repository, processer ReviewProcesser) http.Handler {
@@ -40,25 +50,25 @@ func wbHandler(repository Repository, processer ReviewProcesser) http.Handler {
 			fmt.Printf("Error was: %v\n", err)
 		}
 
-		for _, review := range processedReviews {
-			product, photos := ConvertIntoDomainProduct(review)
-			domainReview := ConvertIntoDomainReview(review)
-			err := repository.AddProduct(
-				product,
-				photos...,
-			)
-			if err != nil {
-				fmt.Printf("Couldnt add the product to the db: %v\n", err)
-			}
+		// for _, review := range processedReviews {
+		// 	product, photos := ConvertIntoDomainProduct(review)
+		// 	domainReview := ConvertIntoDomainReview(review)
+		// 	err := repository.AddProduct(
+		// 		product,
+		// 		photos...,
+		// 	)
+		// 	if err != nil {
+		// 		fmt.Printf("Couldnt add the product to the db: %v\n", err)
+		// 	}
 
-			err = repository.AddReview(
-				domainReview,
-				product,
-			)
-			if err != nil {
-				fmt.Printf("Couldnt add the review to the db: %v\n", err)
-			}
-		}
+		// 	err = repository.AddReview(
+		// 		domainReview,
+		// 		product,
+		// 	)
+		// 	if err != nil {
+		// 		fmt.Printf("Couldnt add the review to the db: %v\n", err)
+		// 	}
+		// }
 
 		values := google_sheets.PrepareDataForSheets(processedReviews)
 		if err := google_sheets.WriteReviewsToSheet(values, spreadsheetID, sheetRange, credentialsFile); err != nil {
@@ -68,4 +78,69 @@ func wbHandler(repository Repository, processer ReviewProcesser) http.Handler {
 
 		w.WriteHeader(http.StatusOK)
 	})
+}
+
+func ValidateOrCreateGigachatAccessKey(h http.Handler) http.Handler {
+	now := time.Now()
+	timeDifference := now.Unix() - gigachatAccessKeyCreationTime.Unix()
+	if !(timeDifference <= 30*60) {
+		url := "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
+		data := "scope=GIGACHAT_API_PERS"
+
+		req, err := http.NewRequest("POST", url, bytes.NewBufferString(data))
+		if err != nil {
+			fmt.Errorf("Error creating request: %v", err)
+		}
+
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		req.Header.Set("Accept", "application/json")
+
+		rquid, err := uuid.NewRandom()
+		if err != nil {
+			fmt.Printf("Error generating UUID: %v", err)
+		}
+		req.Header.Set("RqUID", rquid.String())
+
+		authKey := os.Getenv("GIGACHAT_AUTH_KEY")
+		if authKey == "" {
+			fmt.Printf("GIGACHAT_AUTH_KEY environment variable not set")
+		}
+		req.Header.Set("Authorization", "Basic "+authKey)
+
+		client := &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			},
+		}
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Printf("Error sending request: %v", err)
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			fmt.Printf("Please forgive: %v", resp.StatusCode)
+		}
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Printf("Error reading response: %v", err)
+		}
+		type Response struct {
+			AccessToken string `json:"access_token"`
+		}
+		var response Response
+		err = json.Unmarshal(body, &response)
+
+		if err != nil {
+			fmt.Printf("Error decoding response")
+		}
+		// os.Setenv("GIGACHAT_ACCESS_TOKEN", response.AccessToken)
+		file, _ := os.Open(".env")
+		envVariables, _ := godotenv.Parse(file)
+		envVariables["GIGACHAT_ACCESS_TOKEN"] = response.AccessToken
+		godotenv.Write(envVariables, file.Name())
+		godotenv.Load()
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { h.ServeHTTP(w, r) })
 }
